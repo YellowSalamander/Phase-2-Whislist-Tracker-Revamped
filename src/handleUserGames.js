734f90c231f6e031274cbf6a477f18519c4d256a
Samuel Rivero <samuelfras@gmail.com>
2023-09-26 19:54:04 -0600

    Phew, that was quite the work, but now we have renders! yes!

--- src/handleUserGames.js
+++ src/handleUserGames.js
@@ -10,33 +10,45 @@ function HandleUserGames(selectedGames) {
       .then((response) => response.json())
       .then((data) => {
         // Step 2: Extract the fetched data
-        const userGames = data.User;
-        console.log(userGames);
-  
-        // Step 3: Make a GET request to the CheapShark API
-        const fetchPromises = userGames.map((userGame) => {
-          const gameID = userGame.gameID;
-          return fetch(`https://www.cheapshark.com/api/1.0/games?id=${gameID}`).then((response) =>
-            response.json()
-          );
-        });
-  
-        Promise.all(fetchPromises)
-          .then((cheapSharkDataArray) => {
-            console.log(cheapSharkDataArray);
-          })
-          .catch((error) => {
-            console.error('Error from CheapShark API:', error);
+        const userGames = data;
+        const gameID = userGames.map(game => game.gameID)
+        // console.log('this is data.user:', gameID);
+        // console.log('this is data:', data)
+        
+        const fetchData = async () => {
+          const promises = gameID.map(async (id) => {
+            const response = await fetch (`https://www.cheapshark.com/api/1.0/games?id=${id}`)
+            if (!response.ok){
+              throw new Error(`Fetch failed for game ID ${id}`)
+            }
+            const data = await response.json()
+            // console.log('this is the second fetch:', data)
+            return data
           });
+          const gameDataList = await Promise.all(promises);
+          setCheapSharkGameData(gameDataList)
+          console.log('this is dataList:', gameDataList)
+        }
+        fetchData();
+        console.log('this is the setCheap:',cheapSharkGameData)
       })
-      .catch((error) => {
-        console.error('Error fetching data from db.json:', error);
-      });
-  }, []);
-
+    }, [] )
+    console.log('this is the setCheap2:',cheapSharkGameData)
   return (
-    <div>
-
+    <div className='RenderedGames'>
+      <h1>Your Wishlist!</h1>
+      <ul>
+        {cheapSharkGameData.map((game, index)=> (
+          
+          <li key={index} className='GameTitleRender'>{game.info.title}
+          <p>Cheapest Price Ever: {game.cheapestPriceEver.price}</p>
+          <p>Current Cheapest Price: {game.deals[0].price}</p>
+          <img src={game.info.thumb} className='RenderGameThumb'></img>
+          <button>Get deal!</button>
+          </li>
+            
+        ))}
+      </ul>
     </div>
   );
 }
